15fe2318c3e1ecfd1eff338066711c8c1602f89d
MellowCo <799478052@qq.com>
2022-11-22 20:45:51 +0800

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import Unocss from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    uni(),
+    // https://github.com/antfu/unocss
+    Unocss(),
+  ],
+})
